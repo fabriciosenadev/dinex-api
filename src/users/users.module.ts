deee07c1586fe913f6c6b680dc0f73bd23ef25f8
Fabricio Sena <fabricior.sena@outlook.com>
2021-08-11 22:25:23 -0300

    remove unused dependecies

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -4,7 +4,6 @@ import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
-import { UserRepository } from './user.repository';
 
 @Module({
     controllers: [UsersController],

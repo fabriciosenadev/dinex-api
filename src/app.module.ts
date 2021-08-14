a34dd9633ae1effb0d65b6ff4d275ad4bb46b38a
Fabricio Sena <fabricior.sena@outlook.com>
2021-08-14 01:59:38 -0300

    add activation module email module and new configs

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,33 +13,58 @@ import { AuthModule } from './auth/auth.module';
 import { CategoryUser } from './categories-users/category-user.entity';
 import { ActivationModule } from './activation/activation.module';
 import { Activation } from './activation/activation.entity';
+import { EmailModule } from './email/email.module';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
 
 @Module({
     imports: [
         ConfigModule.forRoot(),
+        MailerModule.forRoot({
+            transport: {
+                host: process.env.SMTP_HOST,
+                port: parseInt(process.env.SMTP_PORT),
+                secure: false,
+                auth: {
+                    user: process.env.EMAIL_ADDRESS,
+                    pass: process.env.EMAIL_PASS,
+                },
+            },
+            defaults: {
+                from: process.env.EMAIL_NAME + ' <' + process.env.EMAIL_ADDRESS + '>',
+            },
+            template: {
+                dir: process.env.TEMPLATE_PATH,
+                adapter: new HandlebarsAdapter(),
+                options: {
+                    strict: true,
+                }
+            },
+        }),
         TypeOrmModule.forRoot({
             type: 'sqlite',
-            database: 
-                process.env.NODE_ENV === 'test' 
-                ? "./src/database/database.test.db" 
-                :"./dist/database/dinex.db",
+            database:
+                process.env.NODE_ENV === 'test'
+                    ? "./src/database/database.test.db"
+                    : "./dist/database/dinex.db",
             logging: true,
             synchronize: true,
             entities: [User, Category, CategoryUser, Activation],
             migrationsTableName: 'migrations_table',
             migrations: [
-               "./src/database/migrations/**.ts"
+                "./src/database/migrations/**.ts"
             ],
             cli: {
-               "migrationsDir": "src/database/migrations"
+                "migrationsDir": "src/database/migrations"
             }
         }),
-        EptestModule, 
+        EptestModule,
         UsersModule,
         CategoriesModule,
         CategoriesUsersModule,
         AuthModule,
-        ActivationModule
+        ActivationModule,
+        EmailModule
     ],
     // controllers: [AppController],
     // providers: [AppService],

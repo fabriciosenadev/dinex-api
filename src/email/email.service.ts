54a90d31d73967b5572aeae7b3940c4b17c8345c
Fabricio Sena <fabricior.sena@outlook.com>
2021-08-14 01:26:18 -0300

    add email module

---
+++ src/email/email.service.ts
@@ -0,0 +1,40 @@
+import { MailerService } from "@nestjs-modules/mailer";
+import { Injectable } from "@nestjs/common";
+
+@Injectable()
+export class EmailService {
+    constructor(private readonly mailerService: MailerService) { }
+
+    public async sendActivationEmail(activationCode: string, fullName: string, to: string) {      
+        const mailBoxName = process.env.EMAIL_NAME;
+        const mailBoxAddress = '<' + process.env.EMAIL_ADDRESS + '>';
+        const from = `${mailBoxName} ${mailBoxAddress}`;
+        
+        const subject = 'Ativação de Usuário ✔';
+        
+        const templatePath = process.env.TEMPLATE_PATH;
+        const template = `${templatePath}activationUser`;
+        
+        const name = fullName.split(' ')[0];
+
+        let result;
+        this
+            .mailerService
+            .sendMail({
+                to,
+                from,
+                subject,
+                template,
+                context: {
+                    name,
+                    activationCode
+                }
+            })
+            .then((success) => {
+                result = success;
+            }).catch((err) => {
+                result = err;
+            });
+            return result;
+    }
+}

f63fd3124e08ad4c3ad119f970b01589407f5dd5
Fabricio Sena <fabricior.sena@outlook.com>
2021-08-16 21:45:10 -0300

    add new bind standard categories and bind to user

--- src/categories-users/categories-users.service.ts
+++ src/categories-users/categories-users.service.ts
@@ -1,33 +1,50 @@
 import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
+import { Category } from "src/categories/category.entity";
 import { Repository } from "typeorm";
 import { CategoryUser } from "./category-user.entity";
 
 @Injectable()
 export class CategoriesUsersService {
     constructor(
-        @InjectRepository(CategoryUser) private repository: Repository<CategoryUser>
+        @InjectRepository(CategoryUser) private categoryUserRepository: Repository<CategoryUser>
     ) { }
 
     public async addCategoryRelationToUser(categoryId: number, userId: string) {
         try {
-            const relation = await this.repository.create({
+            const relation = await this.categoryUserRepository.create({
                 category_id: categoryId,
                 user_id: userId
             });
-            await this.repository.save(relation);
+            await this.categoryUserRepository.save(relation);
         } catch (error) {
             console.log(error);
             const msg = "Error adding category relation to user";
-            throw new HttpException({status: 500, error: msg}, HttpStatus.INTERNAL_SERVER_ERROR);
+            throw new HttpException({ status: 500, error: msg }, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
     public async findCategoryRelationToUser(categoryId: number, userId: string) {
-        return await this.repository.findOne({
+        return await this.categoryUserRepository.findOne({
             category_id: categoryId,
             user_id: userId,
             deleted_at: null
         });
+    }
+
+    public async addStandardCategoriesToUser(userId: string, standardCategories: Category[]) {
+        try {
+            for (const standardCategory of standardCategories) {
+                const relation = await this.categoryUserRepository.create({
+                    category_id: standardCategory.id,
+                    user_id: userId
+                });
+                await this.categoryUserRepository.save(relation);
+            }
+        } catch (error) {
+            console.log(error);
+            const msg = "Error adding standard categories to user";
+            throw new HttpException({ status: 500, error: msg }, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -4,6 +4,7 @@ import { CategoriesUsersService } from "src/categories-users/categories-users.se
 import { IsCustom } from "./enums/iscustom";
 import { Category } from "./category.entity";
 import { CategoryRepository } from "./category.repository";
+import { Applicable } from "./enums/applicable";
 
 @Injectable()
 export class CategoriesService {
@@ -43,11 +44,82 @@ export class CategoriesService {
 
     }
 
+    public async bindStandardCategories(userId: string) {
+        const hasStandardCategories = await this.hasStandardCategories();
+        if (!hasStandardCategories) {
+            await this.AddStandardCategories();
+        }
+
+        const standardCategories = await this.categoryRepository.find({
+            is_custom: IsCustom.No
+        });
+
+        this.categoriesUsersService.addStandardCategoriesToUser(userId, standardCategories);
+    }
+
     private async addCategoryRelationToUser(categoryId: number, userId: string) {
         const alreadyExists = await this.categoriesUsersService.findCategoryRelationToUser(categoryId, userId);
         if (alreadyExists)
             throw new HttpException({ status: 409, error: "Categoria ja existe" }, HttpStatus.CONFLICT);
 
         await this.categoriesUsersService.addCategoryRelationToUser(categoryId, userId);
+    }
+
+    private async hasStandardCategories() {
+        const allStandardCategories = await this.categoryRepository.find({
+            is_custom: IsCustom.No
+        });
+        return allStandardCategories.length > 0;
+    }
+
+    private async AddStandardCategories() {
+        const salary = await this.categoryRepository.create({
+            name: "Salário",
+            applicable: Applicable.In,
+            is_custom: IsCustom.No,
+        })
+        await this.categoryRepository.save(salary);
+
+        const food = await this.categoryRepository.create({
+            name: "Alimentação",
+            applicable: Applicable.Out,
+            is_custom: IsCustom.No,
+        })
+        await this.categoryRepository.save(food);
+
+        const beauty = await this.categoryRepository.create({
+            name: "Beleza",
+            applicable: Applicable.Out,
+            is_custom: IsCustom.No,
+        })
+        await this.categoryRepository.save(beauty);
+
+        const education = await this.categoryRepository.create({
+            name: "Educação",
+            applicable: Applicable.Out,
+            is_custom: IsCustom.No,
+        })
+        await this.categoryRepository.save(education);
+
+        const laser = await this.categoryRepository.create({
+            name: "Lazer",
+            applicable: Applicable.Out,
+            is_custom: IsCustom.No,
+        })
+        await this.categoryRepository.save(laser);
+
+        const health = await this.categoryRepository.create({
+            name: "Saúde",
+            applicable: Applicable.Out,
+            is_custom: IsCustom.No,
+        })
+        await this.categoryRepository.save(health);
+
+        const transport = await this.categoryRepository.create({
+            name: "Transporte",
+            applicable: Applicable.Out,
+            is_custom: IsCustom.No,
+        });
+        await this.categoryRepository.save(transport);
     }
 }

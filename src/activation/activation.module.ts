6da4c083129f3ec559129c607ec0958574643820
Fabricio Sena <fabricior.sena@outlook.com>
2021-08-16 21:44:12 -0300

    add new structure to activation module

--- src/activation/activation.module.ts
+++ src/activation/activation.module.ts
@@ -1,14 +1,28 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { CategoriesModule } from 'src/categories/categories.module';
+import { EmailModule } from 'src/email/email.module';
 import { UsersModule } from 'src/users/users.module';
 import { ActivationController } from './activation.controller';
 import { Activation } from './activation.entity';
+import { ActivationMiddleware } from './activation.middleware';
 import { ActivationService } from './activation.service';
 
 @Module({
     controllers: [ActivationController],
-    imports: [TypeOrmModule.forFeature([Activation]), UsersModule],
+    imports: [
+        TypeOrmModule.forFeature([Activation]),
+        UsersModule,
+        EmailModule,
+        CategoriesModule,
+    ],
     providers: [ActivationService],
     exports: [ActivationService],
 })
-export class ActivationModule {}
+export class ActivationModule implements NestModule {
+    configure(consumer: MiddlewareConsumer) {
+        consumer
+            .apply(ActivationMiddleware.validateActivation)
+            .forRoutes({ path: 'activate/', method: RequestMethod.POST });
+    }
+}
